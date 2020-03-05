8bb878be7e1c835b8cccc6fe019b9c5117cbf170
Tapila Hithesh <tapila.hithesh@gmail.com>
2020-03-05 23:43:28 +0530

    Links inside the webview will open now.

--- background.js
+++ background.js
@@ -1,8 +1,22 @@
 chrome.app.runtime.onLaunched.addListener(function () {
+  runApp();
+});
+
+chrome.app.runtime.onRestarted.addListener(function () {
+  runApp();
+});
+
+
+function runApp(){
   chrome.app.window.create('window.html', {
     bounds: {
       'width': 1400,
       'height': 800
     },
-  });
-});
+  }, onWindowLoaded());
+}
+
+function onWindowLoaded(popup) {
+  return function (win) {
+  };
+}
--- main.js
+++ main.js
@@ -2,13 +2,50 @@ function updateWebviews() {
   var webview = document.querySelector("webview");
   webview.style.height = document.documentElement.clientHeight + "px";
   webview.style.width = document.documentElement.clientWidth + "px";
-
-
+  return webview;
 };
 
 function loaded() {
-  updateWebviews();
+  var webview = updateWebviews();
+  webview.addEventListener('newwindow', onNewWindow);
 }
 
+
+function onNewWindow(event) {
+  switch (event.windowOpenDisposition) {
+    case 'ignore':
+      // Not sure what this is used by.  Default enum value, maybe.
+      console.debug('Ignoring new window request');
+      return;
+
+    case 'save_to_disk':
+      // Ctrl + S, maybe?  Not sure how to reproduce that.
+      console.log('save_to_disk is not implemented');
+      return;
+
+    case 'current_tab':
+      console.log("xiaofeng: ==================== current_tab");
+      return;
+
+    case 'new_background_tab':
+    case 'new_foreground_tab':
+      newWindow = open(event.targetUrl, '_blank');
+      if (event.windowOpenDisposition != 'new_background_tab') {
+        newWindow.focus();
+      }
+      break;
+
+    case 'new_window':
+    case 'new_popup':
+      // if (event.initialWidth && event.initialHeight) {
+      //   features = 'width=' + event.initialWidth + ',height=' + event.initialHeight;
+      // }
+      newWindow = open(event.targetUrl, '_blank');
+      newWindow.focus();
+      break;
+  }
+}
+
+
 onload = loaded;
-window.onresize = loaded;
+window.onresize = updateWebviews;
